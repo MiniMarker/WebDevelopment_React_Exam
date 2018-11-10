7555c4ae11dde37dfe8120530d6e26057956699e
MiniMarker <cfmarker@gmail.com>
2018-11-10 13:29:10 +0100

    take all users now works, also changed entity classes to entityRepositories

---
+++ server/db/quizRepository.js
@@ -0,0 +1,46 @@
+/*
+let Game = {
+	// liste over spillere
+	id: uuid,
+	name: "",
+	players: [username1, username2, username3]
+
+};
+*/
+
+const uuid = require('uuid');
+
+let quizzes = new Map();
+
+const createQuiz = (question, ans1, ans2, ans3, ans4, correctAns) => {
+
+	const quiz = {
+		id: uuid(),
+		question,
+		ans1,
+		ans2,
+		ans3,
+		ans4,
+		correctAns
+	};
+
+	quizzes.set(quiz.id, quiz);
+};
+
+const getAllQuizzes = () => {
+	return Array.from(quizzes.entries());
+};
+
+const getQuiz = (id) => {
+	return quizzes.get(id);
+};
+
+const getRandomQuiz = () => {
+
+	const randomNum = Math.floor(Math.random() * quizzes.size);
+
+	return Array.from(quizzes.entries())[randomNum]
+};
+
+
+module.exports = {createQuiz, getQuiz, getAllQuizzes, getRandomQuiz};
